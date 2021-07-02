3cdcc780adb021cd6da6d8cb8e742024f962ffda
Darshan <rl.darshan01@gmail.com>
2021-07-02 16:33:50 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, Directive, ElementRef, HostListener } from '@angular/core';
+
+@Pipe({
+  name: 'currencyWithoutDecimal'
+})
+export class currencyWithoutDecimalPipe {
+  transform(val: string) {
+    return new Intl.NumberFormat('en-IN', {
+      style: 'currency',
+      currency: 'INR',
+      minimumFractionDigits: 0,
+    }).format(Number(val));
+
+  }
+}
+
+
+@Directive({
+  selector: 'input[numbersOnly]'
+})
+export class NumberDirective {
+
+  constructor(private _el: ElementRef) { }
+
+  @HostListener('input', ['$event']) onInputChange(event: any) {
+    const initalValue = this._el.nativeElement.value;
+    this._el.nativeElement.value = initalValue.replace(/[^0-9]*/g, '');
+    if (initalValue !== this._el.nativeElement.value) {
+      event.stopPropagation();
+    }
+  }
+}
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +38,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'openFinTechTask';
+  monthlyIncome = "150000";
+  monthlyExpense = "0";
+  haveExistingLoan = true;
+  repaymentTenure = 'yearly';
+  existingLoanAmount!: number;
+
+  updateLoan() {
+    // Code here
+  }
+
+  updatedExpense() {
+    // Code here
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, NumberDirective, currencyWithoutDecimalPipe } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NumberDirective,
+    currencyWithoutDecimalPipe
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+
+    MatIconModule,
+    MatListModule,
+    MatInputModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
